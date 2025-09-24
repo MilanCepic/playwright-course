03aa781bed327944ccf0de72096632c6d835266c
Milan Čepić <milancepic@Milans-Mac-mini.local>
2025-09-24 19:46:21 +0200

    13 lekcija

--- tests/new_user_full_journie.spec.js
+++ tests/new_user_full_journie.spec.js
@@ -1,8 +1,12 @@
 import { test } from "@playwright/test";
-import { ProductsPage } from "./../page-objects/ProductPage.js";
+import { ProductsPage } from "../page-objects/ProductsPage.js";
 
 test.only("New user full end-to-end test journey", async ({ page }) => {
   const productsPage = new ProductsPage(page);
   await productsPage.visit();
+
+  await productsPage.addProductToBasket(0);
+  await productsPage.addProductToBasket(1);
+  await productsPage.addProductToBasket(2);
   await page.pause();
 });
